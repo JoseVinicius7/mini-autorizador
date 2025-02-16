344fa551d4ccc6369f723a0eb691d7a3cde79ac9
José Vinicius <jose.vinicius7@hotmail.com>
2025-02-16 13:33:48 -0300

    criar endpoints REST para Cartao

---
+++ src/main/java/com/br/authorizer/controller/CartaoController.java
@@ -0,0 +1,29 @@
+package com.br.authorizer.controller;
+
+import com.br.authorizer.entity.CartaoEntity;
+import com.br.authorizer.service.CartaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/cartoes")
+@RequiredArgsConstructor
+public class CartaoController {
+
+    private final CartaoService cartaoService;
+
+    @PostMapping
+    public ResponseEntity<CartaoEntity> criarCartao(@RequestBody CartaoEntity cartao) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartaoService.criarCartao(cartao));
+    }
+
+    @GetMapping("/{numeroCartao}")
+    public ResponseEntity<BigDecimal> obterSaldo(@PathVariable String numeroCartao) {
+        return ResponseEntity.ok(cartaoService.obterSaldo(numeroCartao));
+    }
+}
+
